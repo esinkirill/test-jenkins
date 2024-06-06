4dcaa318aabb04914a25f9e543950c82feb762b8
esinkirill <esin187@mail.ru>
2024-06-07 01:00:45 +0300

    tets

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hi,    World!'
+def hello():
+    return 'hello'
 
 if __name__ == '__main__':
-    app.run(host='188.120.225.17', port=5003)
+    app.run(host='0.0.0.0', port=8085)
 
